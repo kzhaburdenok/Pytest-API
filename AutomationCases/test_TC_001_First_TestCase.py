1f656518052035c072d35ee53ecf492810dc31b4
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-05-12 19:30:46 +0300

    decorators are added

--- AutomationCases/test_TC_001_First_TestCase.py
+++ AutomationCases/test_TC_001_First_TestCase.py
@@ -1,8 +1,12 @@
 import pytest
 
+a = 101
+#@pytest.mark.skipif(a>100, reason='skipping as this func is not working')
+@pytest.mark.new()
 def test_tc_001_login_logout_testing():
     print("\nkek")
 
-
+@pytest.mark.new()
+@pytest.mark.necessary()
 def test_tc_003_login_logout_ivalid_credentials():
     print("\nkek3")
